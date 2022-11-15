d849be74432d0e828e7be55980d59a4004a1ec36
KakaCarrotCake <rin002@ucsd.edu>
2022-11-15 14:00:10 -0800

    An attempt at an autograder

--- TestListExamples.java
+++ TestListExamples.java
@@ -1,6 +1,20 @@
+import org.junit.Test;
+import java.util.ArrayList;
+import java.util.List;
 import static org.junit.Assert.*;
-import org.junit.*;
 
 public class TestListExamples {
   // Write your grading tests here!
+  @Test
+    public void testFilter() {
+        List<String> test = new ArrayList<String>();
+        test.add("Guuci");
+        test.add("Good");
+        test.add("Great");
+        List<String> expect = new ArrayList<String>();
+        expect.add("Guuci");
+        expect.add("Great");
+        List<String> result = ListExamples.filter(test, new LongString());
+        assertEquals(expect, result);
+    }
 }
--- grade.sh
+++ grade.sh
@@ -1,7 +1,14 @@
 # Create your grading script here
 
+CPATH=".:lib/hamcrest-core-1.3.jar:lib/junit-4.13.2.jar"
 set -e
 
 rm -rf student-submission
 git clone $1 student-submission
-
+if () {
+    echo "ListExamples.java was not submitted.";
+    exit;
+}
+cd 
+javac -cp $CPATH *.java
+java -cp $CPATH org.junit.runner.JUnitCore 
